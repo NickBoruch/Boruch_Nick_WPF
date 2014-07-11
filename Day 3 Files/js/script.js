e94eba44dee2abab54df62d1ceefb65408eacaaf
NickBoruch <nickboruch@Nicks-MacBook-Pro-7.local>
2014-07-11 18:22:31 -0400

    Setup First Files
    Created html and js files
    using web storm

---
+++ Day 3 Files/js/script.js
@@ -0,0 +1,16 @@
+/*
+Nick Boruch
+WPF 1407
+7-11-2014
+Day 3 First File
+ */
+
+/*
+This is an example of a multi-lined comment
+Anything in-between the star and slash will not be effected.
+ */
+
+
+
+
+alert("love this class");
