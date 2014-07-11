2bd931acfaa1a751f2f9bc3f811bb388057bcc8b
NickBoruch <nickboruch@Nicks-MacBook-Pro-7.local>
2014-07-11 18:33:03 -0400

    Covered Comments Alert and Console
    Covered Comments Alert and Console

--- Day 3 Files/js/script.js
+++ Day 3 Files/js/script.js
@@ -10,7 +10,12 @@ This is an example of a multi-lined comment
 Anything in-between the star and slash will not be effected.
  */
 
+// Single Line Comment
 
+// Example Comment goes here
 
-
+// alert () - creates a pop up box that the user can see
 alert("love this class");
+
+// Console.log() - coders to help troubleshoot code
+console.log("This is a Console statement");
