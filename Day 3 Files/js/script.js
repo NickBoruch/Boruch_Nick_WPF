e183e0d0a1ff349067bc940e4a0e96d0dbc782af
NickBoruch <nickboruch@Nicks-MacBook-Pro-7.local>
2014-07-11 19:35:51 -0400

    Creating Variables
    fun with storage boxes

--- Day 3 Files/js/script.js
+++ Day 3 Files/js/script.js
@@ -19,3 +19,45 @@ alert("love this class");
 
 // Console.log() - coders to help troubleshoot code
 console.log("This is a Console statement");
+
+
+// Declare a variable
+// Use the keyword var
+var whatever1;  //Declaration of a variable
+whatever1 = 42;  //Definition of a variable - giving value
+
+// storageContainer = Value; - define a variable
+
+//Setup and define the variable in one step
+var a = 2;  // Declaring and Defining in one step
+
+//Check the value of the a variable
+console.log(a);
+
+//How do we add in JS? +
+
+//THIS IS NOT CORRECT!!!
+a+3; // temporary doesnt track it
+a= a+3;
+console.log(a);
+
+console.log(a+3);
+
+//a=3;
+
+console.log(a);
+
+
+//Declare a new variable
+var b;
+
+//Define b, using variable a
+
+b= a+3;
+
+console.log(b);
+console.log(a);
+
+
+
+
