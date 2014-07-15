1545a37edb6c02032b34800bfbf0288a3c8e5e4a
NickBoruch <nickboruch@Nicks-MacBook-Pro-7.local>
2014-07-14 22:23:04 -0400

    Personal Expressions
    Expressions Homework:Personal

---
+++ Boruch_Nicholas_Expressions/js/expressionsPersonal.js
@@ -0,0 +1,22 @@
+
+ /*
+Nick Boruch
+WPF 1407
+7/14/14
+Personal Expressions
+ */
+
+//alert("Testing 1,2,3");
+
+
+// How Much I Spend on iTunes each year
+
+var userInput= prompt("How much money do you spend on iTunes each week? ");
+console.log(userInput);
+
+var userInput= prompt("How Much money do you spend on iTunes each Month? ");
+console.log (userInput);
+
+var userInput= prompt("How Much Money you spend each year on iTunes? ");
+console.log(userInput);
+
