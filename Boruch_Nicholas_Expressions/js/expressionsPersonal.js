c4efb1818ac29e103b3ae33fe1f4fb3a4915691a
NickBoruch <nickboruch@Nicks-MacBook-Pro-7.local>
2014-07-18 17:27:33 -0400

    Day 6 Files
    Conditionals Reviews

--- Boruch_Nicholas_Expressions/js/expressionsPersonal.js
+++ Boruch_Nicholas_Expressions/js/expressionsPersonal.js
@@ -11,12 +11,18 @@ Personal Expressions
 
 // How Much I Spend on iTunes each year? I spend $10 a week on iTunes
 
-var userInput= prompt("How much money do you spend on iTunes each week? ");
+var userInput= prompt(" How much money do you spend on iTunes each week? ");
 console.log(userInput);
 
 var userInput= Number (userInput)* 4;
-console.log ("You Spend " +userInput+" dollars a month on iTunes. " );
+console.log (" You Spend " +userInput+" dollars a month on iTunes. " );
 
 var userInput= Number (userInput)* 52;
 console.log(" You Spend " +userInput+ " dollars a year on iTunes. ");
+
+
+
+
+
+
 
