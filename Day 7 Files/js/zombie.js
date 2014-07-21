f397529c4ad5a5886b4483ac16a80ce56f242f75
NickBoruch <nickboruch@Nicks-MacBook-Pro-7.local>
2014-07-21 19:03:25 -0400

    Zombie OUTBREAK
    long story short we die

---
+++ Day 7 Files/js/zombie.js
@@ -0,0 +1,33 @@
+
+ /*
+Nick Boruch
+WPF 1407
+7/21/14
+Zombie Outbreak
+ */
+
+//alert("Testing 1,2,3");
+
+//We have a zombie here at Full Sail.
+ //It can bite 4 people a day and turn them into zombies the following day.
+
+//The CDC wants to know how many zombies there will be in 8 days.
+
+//Givens
+var numZombies = 1; //How many zombies do we have
+var numBites= 4; //Number of bites per zombie per day
+var days = 10; //Number of days the CDC wants to know
+
+for(var i = 1; i<=days; i++){
+
+  //How many new zombies get made  each day
+  var newZombies = numZombies * numBites;
+
+ //How many zombies do we have
+ numZombies = numZombies + newZombies; //numZombies += newZombies;
+
+ //Console.log
+ console.log("At the end of day #"+i+" there are "+numZombies+" zombies!");
+
+}
+
