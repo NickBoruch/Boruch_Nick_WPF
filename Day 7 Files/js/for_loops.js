d74be8f7fdb1e41cc39225b75b360fefcff7b79c
NickBoruch <nickboruch@Nicks-MacBook-Pro-7.local>
2014-07-21 18:11:28 -0400

    Break Time
    Break Time

--- Day 7 Files/js/for_loops.js
+++ Day 7 Files/js/for_loops.js
@@ -17,3 +17,16 @@ For Loops
 for(var i=0; i<20; i++){
     console.log("i is "+i);
 }
+
+//Break
+
+ for(var j=0; j<5; j++){
+
+     if(j===3){
+         break;
+
+     }
+
+console.log("j = "+j);
+
+ }
