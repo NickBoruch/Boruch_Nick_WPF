b3d85e24e8adeb18685d54be4a32b03d0fd27670
NickBoruch <nickboruch@Nicks-MacBook-Pro-7.local>
2014-07-22 03:03:08 -0400

    Expressions:Industry
    edited the Industry specific part of the expressions homework

---
+++ Boruch_Nicholas_Expressions/js/expressions_industry.js
@@ -0,0 +1,20 @@
+
+ /*
+Nick Boruch
+WPF 1407
+7/14/14
+Expressions:Industry
+ */
+
+//alert("Testing 1,2,3");
+
+// Finding out how much money total people owe on bills
+
+var moneyOwed =[1200,10000,5000];
+
+// Find out how much money is owed on 3 jobs
+
+var totalMoney= moneyOwed[0]+ moneyOwed [1]+ moneyOwed [2];
+console.log ("The total money that people owe you is "+totalMoney+ " dollars!" );
+
+//Total owed is 16200 dollars
