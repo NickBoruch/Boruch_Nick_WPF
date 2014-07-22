59733e73879d9b9988c5722f82d0a12a22e67adc
NickBoruch <nickboruch@Nicks-MacBook-Pro-7.local>
2014-07-21 22:22:24 -0400

    Loops worksheet
    While Loop & Do While Loop

--- Boruch_Nicholas_Loops Worksheet/js/loops_worksheet.js
+++ Boruch_Nicholas_Loops Worksheet/js/loops_worksheet.js
@@ -6,4 +6,30 @@ WPF 1407
 Loops Worksheet
  */
 
-alert("Testing 1,2,3");
+//alert("Testing 1,2,3");
+
+//While Loop
+
+//My Counter variable =music
+
+var music = 1;
+
+while (music<500){
+
+  console.log ("Music Never Sleeps! It is what makes us dance! "+music);
+    music++
+}
+console.log("Never give up on your dreams! \nYou can accomplish anything you set your mind too!"+music);
+
+// Do While Loop
+
+// My counter variable is i=200
+
+var i = 200
+
+ do{
+     console.log("You spend "+i+ " Dollars on music");
+     i++
+
+ }while (i<200);
+
