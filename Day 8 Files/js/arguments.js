454ed2e4bb52cd4c72c4a040ba39f45e1325828b
NickBoruch <nickboruch@Nicks-MacBook-Pro-7.local>
2014-07-23 19:12:01 -0400

    Arguments & parameters are working
    Arguments & parameters are working

--- Day 8 Files/js/arguments.js
+++ Day 8 Files/js/arguments.js
@@ -13,19 +13,37 @@ Arguments
 
 // Function to calculate the area of a rectangle
 
-function calcArea(){
+//w,h are parameters - Same as declaring variables
+//These variables are scoped to the function calcArea
+function calcArea(w, h){
 
     //Create variables for width, height & area
 
-    var width = 20;
-    var height= 10;
-    var area = width * height
+    //HARDCODED VALUES----No BUENO!
+    //var width = 20;
+    //var height= 10;
+    //var area = width * height
+
+    //Create the variable, but using the parameters
+    var area =w*h;
+
+    //You have twp of the same rectangles, whats the area of both
+    var areaBoth=area *2;
+
 
     //Console.log out the area
     console.log (area);
-
+    console.log (areaBoth+ " area of two rectangles");
 }
 
 //Call the function
+//calcArea();
 
-calcArea();
+
+//Create function call with Arguments
+calcArea(20,10);
+
+ calcArea (5,6);
+
+//Let's add in arguments and parameters
+//This will make the function more flexible
