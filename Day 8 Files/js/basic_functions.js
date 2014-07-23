4c7f3906b3d9290a58a54413d7c974eaa825016b
NickBoruch <nickboruch@Nicks-MacBook-Pro-7.local>
2014-07-23 18:01:26 -0400

    Basic Function
    Basic Function

---
+++ Day 8 Files/js/basic_functions.js
@@ -0,0 +1,51 @@
+
+ /*
+Nick Boruch
+WPF 1407
+7/23/14
+Basic Functions
+ */
+
+//alert("Testing 1,2,3");
+
+//Basic Function
+//Console.log to the user
+
+function printHello(){
+
+    console.log("Functions are FUNctions!");
+
+    }
+
+ function printMore(){
+
+     console.log("Prints out more text!");
+ }
+
+
+//Function call to actually run the code
+//"Go Button"
+
+printHello();
+printHello();
+
+printMore();
+ 
+ 
+ 
+ 
+ 
+ 
+//Create a function that will calculate the area of a rectangle
+
+ function blue() {
+     //Create variables for width, height and area
+     var width = 20;
+     var height = 30;
+     var area = width * height;
+
+     //Console.log
+     console.log (area);
+ }
+
+ blue();
