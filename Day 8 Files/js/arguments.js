42604e2c13aee42062a922470807ebba2a004a02
NickBoruch <nickboruch@Nicks-MacBook-Pro-7.local>
2014-07-23 18:59:46 -0400

    Basic Area function
    no arguments or parameters yet

---
+++ Day 8 Files/js/arguments.js
@@ -0,0 +1,31 @@
+
+ /*
+Nick Boruch
+WPF 1407
+7/23/14
+Arguments
+ */
+
+//alert("Testing 1,2,3");
+
+//Arguments - > In Function Call -> Goes into the function
+//Parameters -> in function definition -> Catch the incoming arguments
+
+// Function to calculate the area of a rectangle
+
+function calcArea(){
+
+    //Create variables for width, height & area
+
+    var width = 20;
+    var height= 10;
+    var area = width * height
+
+    //Console.log out the area
+    console.log (area);
+
+}
+
+//Call the function
+
+calcArea();
