f2c64170f60353795b1b6916d3e9c936bf6e15e4
NickBoruch <nickboruch@Nicks-MacBook-Pro-7.local>
2014-07-23 20:18:50 -0400

    How much is pizza
    How much is pizza

--- Day 8 Files/js/return.js
+++ Day 8 Files/js/return.js
@@ -35,3 +35,55 @@ var results = calcArea(width,height);
 console.log("The area of your rectangle is "+results);
 
 
+//Create a function for calculating the area of a circle
+
+function circleArea(r){
+
+//Calc Area
+    var area = 2*r* Math.PI
+
+    //Return the area
+    return area;
+
+}
+
+
+
+ //Ask the user for a radius
+ var radius1 = prompt("Let's calc the area of a circle!\nType in a radius:");
+
+ //Function call
+var firstArea = circleArea(radius1);
+ console.log("The area of a circle with a radius of "+radius1+" is "+firstArea.toFixed(2));
+
+
+ //Prompt the user for another circle
+ var radius2 =prompt("Please enter another radius:");
+
+ //Function call
+ var secondArea= circleArea(radius2);
+
+ //Console.log the results
+ console.log("The area of a circle with a radius of "+radius2+" is "+secondArea.toFixed(2));
+
+ //Pizza how much is it per sq in
+
+ // area of pizza divided by cost
+
+
+ var pizzaRadius = prompt("What is the radius of your pizza?");
+
+ var pizzaCost = prompt("How much was your pizza?");
+
+ //Use our existing function to find the area of our pizza
+ var pizzaArea = circleArea(pizzaRadius);
+
+ //Variable for cost of pizza per sq inch
+ var pizzaperInch = pizzaArea / pizzaCost
+
+ //console.log
+ console.log ("Your pizza is $"+pizzaperInch.toFixed(2)+" per sq inch");
+
+
+
+
