972cdf98d554b27283954334551010b82578344e
NickBoruch <nickboruch@Nicks-MacBook-Pro-7.local>
2014-07-25 19:52:16 -0400

    anonymous functions
    anonymous functions

---
+++ Day 9 Files/js/anon_function.js
@@ -0,0 +1,19 @@
+
+ /*
+Nick Boruch
+WPF 1407
+7/25/14
+Anonymous Function
+ */
+
+//alert("Testing 1,2,3");
+
+
+//Anonymous Functions
+//Extremely popular in jQuery and js
+//Important in Object Oriented Programming
+//Both styles of functions work
+//Coding style
+//Very quick and easy!
+
+
