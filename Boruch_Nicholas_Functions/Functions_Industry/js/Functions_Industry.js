2e09a5d2edf103c3db4c94f2bb756b3b7aa5801c
NickBoruch <nickboruch@Nicks-MacBook-Pro-7.local>
2014-07-25 22:52:26 -0400

    Functions:Industry
    did an anonymous function

--- Boruch_Nicholas_Functions/Functions_Industry/js/Functions_Industry.js
+++ Boruch_Nicholas_Functions/Functions_Industry/js/Functions_Industry.js
@@ -2,8 +2,26 @@
  /*
 Nick Boruch
 WPF 1407
-7/14/14
-Arrays
+7/25/14
+Functions:Industry
  */
 
-alert("Testing 1,2,3");
+//alert("Testing 1,2,3");
+
+//Making an Anonymous Function
+
+//Solving how much tax should be added to the total charges for your work
+//Setting the variables for the function to t=tax and l=labor
+
+var totalTax = function (t,l) {
+
+    var tax = 10*t+l;
+    return tax;
+
+}
+
+ var total = totalTax(21,10000);
+ console.log (total);
+
+
+
