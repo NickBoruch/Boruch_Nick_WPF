62c2d75055aba17079a46197c6f4940c66ff730a
NickBoruch <nickboruch@Nicks-MacBook-Pro-7.local>
2014-07-25 23:33:13 -0400

    Functions:Wacky
    work continued

--- Boruch_Nicholas_Functions/Functions_Wacky/js/Functions_Wacky.js
+++ Boruch_Nicholas_Functions/Functions_Wacky/js/Functions_Wacky.js
@@ -8,4 +8,21 @@ Functions:Wacky
 
 //alert("Testing 1,2,3");
 
+//Just combining multiple things we have learned into one set of code
+//Musical Albums I own
+ // c=CDs and v=Vinyl
 
+
+ function Albums(c,v){
+
+     var total = c+v;
+     return total;
+ }
+
+
+
+ var totalAlbums= Albums(55,155);
+
+ console.log(totalAlbums);
+
+ 
