97e046c3f196731175577931f9482640804af5b8
NickBoruch <nickboruch@Nicks-MacBook-Pro-7.local>
2014-07-25 23:46:44 -0400

    Functions:Wacky
    edited more and added some things

--- Boruch_Nicholas_Functions/Functions_Wacky/js/Functions_Wacky.js
+++ Boruch_Nicholas_Functions/Functions_Wacky/js/Functions_Wacky.js
@@ -10,19 +10,19 @@ Functions:Wacky
 
 //Just combining multiple things we have learned into one set of code
 //Musical Albums I own
- // c=CDs and v=Vinyl
+ // c=CDs and v=Vinyl d=Destroyed
 
 
- function Albums(c,v){
+ function Albums(c,v,d){
 
-     var total = c+v;
+     var total = c+v-d;
      return total;
  }
 
 
 
- var totalAlbums= Albums(55,155);
+ var totalAlbums= Albums(55,155,15).toFixed(0);
 
  console.log(totalAlbums);
 
- 
+
