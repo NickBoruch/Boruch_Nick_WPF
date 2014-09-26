708d861392665103fbc4aaa6f19f650a2f20ca2f
NickBoruch <nickboruch@Nicks-MacBook-Pro-7.local>
2014-09-26 18:05:08 -0400

    Final Exam
    Final

---
+++ Final Exam WPF/JS/script.js
@@ -0,0 +1,25 @@
+
+ /*
+Nicholas Boruch
+WPF
+9/26/14
+Final Exam WPF
+ */
+//create a function
+
+ function discountPrice(originalCost){
+
+     var itemCost = OriginalCost  *discountPrice()/100;
+     return discount;
+     console.log("the total cost of your item is"+discount+" the original cost was"+originalCost+".")
+ }
+
+ //prompt the user for cost
+
+ var OriginalCost = prompt("What is the cost of them item you are purchasing?");
+
+ //prompt the user for discount %
+
+ var discount = prompt("What is the discount you recieved on the item?");
+
+
